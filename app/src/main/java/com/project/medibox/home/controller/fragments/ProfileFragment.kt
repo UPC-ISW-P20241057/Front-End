c84736cf1d8be047c31a7e98e658cd59a00db8aa
Mauricio Prado <u202010482@upc.edu.pe>
2024-06-11 20:37:25 -0500

    feat: added profile details to profile fragment.

--- app/src/main/java/com/project/medibox/home/controller/fragments/ProfileFragment.kt
+++ app/src/main/java/com/project/medibox/home/controller/fragments/ProfileFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.project.medibox.R
+import com.project.medibox.shared.StateManager
 
 class ProfileFragment : Fragment() {
     override fun onCreateView(
@@ -22,5 +24,17 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        loadProfileData(view)
+    }
+    private fun loadProfileData(view: View) {
+        val tvProfileName = view.findViewById<TextView>(R.id.tvProfileName)
+        val tvProfileLastname = view.findViewById<TextView>(R.id.tvProfileLastname)
+        val tvProfileEmail = view.findViewById<TextView>(R.id.tvProfileEmail)
+        val tvProfileCellphone = view.findViewById<TextView>(R.id.tvProfileCellphone)
+
+        tvProfileName.text = StateManager.loggedUser.name
+        tvProfileLastname.text = StateManager.loggedUser.lastName
+        tvProfileEmail.text = StateManager.loggedUser.email
+        tvProfileCellphone.text = StateManager.loggedUser.phone
     }
 }
