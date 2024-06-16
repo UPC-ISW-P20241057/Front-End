6054a71f637ea454cafd10d4c33817d8f04fe090
Mauricio Prado <u202010482@upc.edu.pe>
2024-06-16 07:54:26 -0500

    refactor: moved medication alarm activities to correct package.

--- app/src/main/java/com/project/medibox/medication/controller/activities/MedicationAlarm.kt
+++ app/src/main/java/com/project/medibox/medication/controller/activities/MedicationAlarm.kt
@@ -1,4 +1,4 @@
-package com.project.medibox.controllers.activities
+package com.project.medibox.medication.controller.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
--- app/src/main/java/com/project/medibox/medication/receivers/MedicineAlarmReceiver.kt
+++ app/src/main/java/com/project/medibox/medication/receivers/MedicineAlarmReceiver.kt
@@ -5,7 +5,7 @@ import android.content.Context
 import android.content.Intent
 
 class MedicineAlarmReceiver: BroadcastReceiver() {
-    override fun onReceive(p0: Context?, p1: Intent?) {
-        TODO("Not yet implemented")
+    override fun onReceive(context: Context, intent: Intent) {
+
     }
 }
