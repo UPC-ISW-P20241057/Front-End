583c8096e74104c0f92ce1f0b1e20b43ae1a7003
Mauricio Prado <u202010482@upc.edu.pe>
2024-06-16 07:48:03 -0500

    feat: added base receiver.

---
+++ app/src/main/java/com/project/medibox/medication/receivers/MedicineAlarmReceiver.kt
@@ -0,0 +1,11 @@
+package com.project.medibox.medication.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class MedicineAlarmReceiver: BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        TODO("Not yet implemented")
+    }
+}
