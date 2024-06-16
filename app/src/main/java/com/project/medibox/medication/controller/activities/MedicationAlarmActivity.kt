995f1f5530515aa7bc6e30f6e824d7ae1c676164
Mauricio Prado <u202010482@upc.edu.pe>
2024-06-16 07:58:00 -0500

    refactor: alarm activities and resources names corrected.

--- app/src/main/java/com/project/medibox/medication/controller/activities/MedicationAlarmActivity.kt
+++ app/src/main/java/com/project/medibox/medication/controller/activities/MedicationAlarmActivity.kt
@@ -7,7 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.project.medibox.R
 
-class MedicationAlarm : AppCompatActivity() {
+class MedicationAlarmActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
--- app/src/main/java/com/project/medibox/medication/controller/activities/MedicationAlarmWithImageActivity.kt
+++ app/src/main/java/com/project/medibox/medication/controller/activities/MedicationAlarmWithImageActivity.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.project.medibox.R
 
-class MedicationAlarm2 : AppCompatActivity() {
+class MedicationAlarmWithImageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_medication_alarm2)
+        setContentView(R.layout.activity_medication_alarm_with_image)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
