c6ba8c53365bf178520b726bb2efdd16eee2c5a3
Mauricio Prado <u202010482@upc.edu.pe>
2024-06-20 18:04:16 -0500

    feat: added menu for voice commands and added permission for recording.

--- app/src/main/java/com/project/medibox/home/controller/activities/HomeActivity.kt
+++ app/src/main/java/com/project/medibox/home/controller/activities/HomeActivity.kt
@@ -1,9 +1,11 @@
 package com.project.medibox.home.controller.activities
 
 import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -23,6 +25,11 @@ import com.project.medibox.shared.AppDatabase
 
 class HomeActivity : AppCompatActivity() {
 
+    companion object {
+        private const val ASR_PERMISSION_REQUEST_CODE = 1
+        private const val NOT_PERMISSION_REQUEST_CODE = 0
+    }
+
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         navigateTo(item)
     }
@@ -36,9 +43,14 @@ class HomeActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(Manifest.permission.POST_NOTIFICATIONS),
-                0
+                NOT_PERMISSION_REQUEST_CODE
             )
         }
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.RECORD_AUDIO),
+            ASR_PERMISSION_REQUEST_CODE
+        )
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bnvMenu)
         bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
@@ -57,6 +69,25 @@ class HomeActivity : AppCompatActivity() {
         }
 
         startServices()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode) {
+            ASR_PERMISSION_REQUEST_CODE -> {
+                if (grantResults[0] == PackageManager.PERMISSION_DENIED)
+                    Toast.makeText(this, "You need to provide microphone permission to invoke voice commands.", Toast.LENGTH_SHORT).show()
+            }
+            NOT_PERMISSION_REQUEST_CODE -> {
+                if (grantResults[0] == PackageManager.PERMISSION_DENIED)
+                    Toast.makeText(this, "You need to provide notification permission to invoke voice commands.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun startServices() {
