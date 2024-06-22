9a44e827ea9d0e606221c3291b8728fc64103f74
Mauricio Prado <u202010482@upc.edu.pe>
2024-06-22 11:48:51 -0500

    added reminder options dialog.

--- app/src/main/java/com/project/medibox/medication/controller/activities/MedicationHistoryActivity.kt
+++ app/src/main/java/com/project/medibox/medication/controller/activities/MedicationHistoryActivity.kt
@@ -1,8 +1,12 @@
 package com.project.medibox.medication.controller.activities
 
+import android.app.Dialog
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,6 +19,7 @@ import com.project.medibox.shared.OnItemClickListener
 
 class MedicationHistoryActivity : AppCompatActivity(), OnItemClickListener<HistoricalReminder> {
     private lateinit var rvMedHistory: RecyclerView
+    private lateinit var reminderDialog: Dialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -37,5 +42,20 @@ class MedicationHistoryActivity : AppCompatActivity(), OnItemClickListener<Histo
     }
 
     override fun onItemClicked(value: HistoricalReminder) {
+        reminderDialog = Dialog(this)
+        reminderDialog.setContentView(R.layout.dialog_reminder_options)
+        reminderDialog.window!!.setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        reminderDialog.window!!.setBackgroundDrawable(AppCompatResources.getDrawable(this, R.drawable.bg_dialog_reminder_options))
+        reminderDialog.setCancelable(true)
+        val btnReminderEdit = reminderDialog.findViewById<Button>(R.id.btnReminderEdit)
+        val btnReminderDelete = reminderDialog.findViewById<Button>(R.id.btnReminderDelete)
+
+        btnReminderEdit.setOnClickListener {
+            reminderDialog.dismiss()
+        }
+        btnReminderDelete.setOnClickListener {
+            reminderDialog.dismiss()
+        }
+        reminderDialog.show()
     }
 }
