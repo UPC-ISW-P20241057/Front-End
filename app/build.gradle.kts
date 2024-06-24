13a4b5b687e50ea4b0d901f9a200ce8f92e2b9ee
Mauricio Prado <u202010482@upc.edu.pe>
2024-06-23 19:23:43 -0500

    feat: added dependency for creating tables.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.converter.gson)
     implementation("com.kizitonwose.calendar:view:2.5.2")
     implementation("org.quanqi:android-holo-graph:0.1.0")
+    implementation("com.github.alcarazolabs:FastTableLayout:1.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
